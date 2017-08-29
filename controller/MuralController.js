91810203babe1f3b6b77d8fc4a412414d4e9e33a
Mario Souto <soutomarios@gmail.com>
2017-08-29 16:49:11 -0300

    Adicionando funcionalidade de retorno xml

--- controller/MuralController.js
+++ controller/MuralController.js
@@ -1,7 +1,8 @@
 'use strict'
 
 var _Promise = require('bluebird')
-	, debug = require('debug')('ceep:controller');
+	, debug = require('debug')('ceep:controller')
+	, xml = require('xml');
 
 function MuralController(DAO) {
 	this.DAO = _Promise.promisifyAll(DAO);
@@ -10,20 +11,45 @@ function MuralController(DAO) {
 MuralController.prototype.help = function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 
-	res.json({instrucoes: [
-	{
-	   "conteudo": "Bem vindo ao Ceep!"
-	   ,"cor": "#FFAA10"
-	}
-	,{
-	   "conteudo": "O site é otimizado para celulares!"
-	   ,"cor": "#45AAEE"
-	}
-	,{
-		"conteudo": "Para mudar o layout, clique no botão Linha do cabeçalho"
-		,"cor": "#FF1010"
-	}
-	]});
+	const objAjudas = {
+		instrucoes: [
+			{
+			"conteudo": "Bem vindo ao Ceep!"
+			,"cor": "#FFAA10"
+			}
+			,{
+			"conteudo": "O site é otimizado para celulares!"
+			,"cor": "#45AAEE"
+			}
+			,{
+				"conteudo": "Para mudar o layout, clique no botão Linha do cabeçalho"
+				,"cor": "#FF1010"
+			}
+	]};
+
+	res.json(objAjudas);
+}
+
+MuralController.prototype.helpXML = function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+	const objAjudas = {
+		instrucoes: [
+			{
+			"conteudo": "Bem vindo ao Ceep!"
+			,"cor": "#FFAA10"
+			}
+			,{
+			"conteudo": "O site é otimizado para celulares!"
+			,"cor": "#45AAEE"
+			}
+			,{
+				"conteudo": "Para mudar o layout, clique no botão Linha do cabeçalho"
+				,"cor": "#FF1010"
+			}
+	]};
+	res.set('Content-Type', 'text/xml');
+	res.send(xml(objAjudas));
 }
 
 MuralController.prototype.saveOrUpdate = function(req, res, next) {
--- routes/cartoes.js
+++ routes/cartoes.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 var mongoose = require('../db/mongoose')
   , MuralDAO = require('../dao/MuralDAO')(mongoose)
-  , MuralController = require('../controller/MuralController')(MuralDAO);
+  , MuralController = require('../controller/MuralController')(MuralDAO)
 
 router.get('/instrucoes', MuralController.help.bind(MuralController));
+
+router.get('/instrucoes/xml', MuralController.helpXML.bind(MuralController));
 
 router.post("/salvar", MuralController.saveOrUpdate.bind(MuralController));
 
